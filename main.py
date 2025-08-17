730bef62fa26ec90b65a14989805d88b0f5ce82f
shikhar <shikhar7531@gmail.com>
2025-08-17 20:05:32 +0530

    python assigment1

---
+++ main.py
@@ -0,0 +1,12 @@
+#task 1
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+print(f"Addition {a} +  {b} = {a+b}")
+print(f"subtraction {a} - {b} = {a-b}")
+print(f"multiplication  {a} * {b} = {a*b}")
+print(f"division {a} / {b} = {a/b}")
+#task 2
+print("\n\n\n\n")
+fname = input("Enter your first name: ")
+lname = input("Enter your last name: ")
+print(f"Hello {fname} {lname}! Welcome to the python program")
